b07315c88c7569575022ce81038de803fec25990
Andrei Korchagin <andrei.korchagin@gmail.com>
2018-03-01 04:24:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 var recipes = new Object();
 
-function updateObjectWithKeyAndValue(object, key, value){
+function updateObjectWithKeyAndValue(object, key, value) {
   var newObj = object;
   newObj.key = value;
   return object;
   return newObj;
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object.key = value;
   return object;
 }
 
-function deleteFromObjectByKey(object, key){
+function deleteFromObjectByKey(object, key) {
   var newObj = object;
   delete newObj.key;
   return object;
   return newObj;
 }
 
-function destructivelyDeleteFromObjectByKey(object, key){
+function destructivelyDeleteFromObjectByKey(object, key) {
   delete object.key;
   return object;
 }
